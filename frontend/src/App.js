db4001873a22a07b4f2125b1c22dd27599749b4d
thejas <thejassathyesh@gmail.com>
2026-03-28 21:17:30 +0530

    Add EduCart frontend with RTK Query

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/bootstrap.custom.css"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { Route, Routes } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main className="py-3">
+      <Container>
+      <Routes>
+        <Route path="/" element={<HomeScreen/>}/>
+        <Route path='product/:id' element={<ProductScreen/>}/>
+      </Routes>
+      </Container>
+      </main>
+
+      <Footer/>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,32 @@
+
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product.jsx'
+import { useGetProductsQuery } from '../slices/productApiSlice.js'
+import Loader from '../components/Loader.js'
+import Message from '../components/Message.js'
+
+function HomeScreen() {
+
+  const { data: products, isLoading, error} = useGetProductsQuery()
+
+
+
+  return (
+    <>
+    {isLoading ? (<Loader/>) : error ? (<Message variant="danger">Error occurred while fetching products.</Message>) : (<><h1>Latest Products</h1>
+    <Row>
+        {products?.map((product)=>{
+            return(
+                <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                <Product product={product}/>
+                </Col>
+            )
+
+
+        })}
+    </Row></>)}
+    </>  
+  )
+}
+
+export default HomeScreen
